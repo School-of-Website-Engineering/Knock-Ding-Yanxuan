10de0fe4f4abdc418d8c71691907c0c790902515
mason369 <1960638223@qq.com>
2023-01-19 15:18:36 +0800

    更新登录状态检测
    Co-Authored-By: JDSA Ling <70191651+LingASDJ@users.noreply.github.com>

--- src/store/userInfo/index.js
+++ src/store/userInfo/index.js
@@ -5,7 +5,7 @@ export default {
 	actions   : {
 		// 获取用户信息
 		async asyncGetUserInfo({ commit }) {
-			const { data: res } = await reqGetUserInfo();
+			let { data: res } = await reqGetUserInfo();
 			commit("setUserInfo", res);
 		}
 	},
@@ -14,6 +14,18 @@ export default {
 		setUserInfo(state, userInfo) {
 			state.userInfo = userInfo.userInfo;
 			state.cartTotal = userInfo.cartTotal;
+		},
+		initUserInfo(state) {
+			(state.cartTotal = 0),
+			//用户信息
+			(state.userInfo = {
+				//用户昵称
+				nickName: "游客",
+				//鸡腿数
+				coin    : 0,
+				//头像
+				headImg : require("../../assets/img/userImg.f8bbec5e.png")
+			});
 		}
 	},
 	//登录状态
