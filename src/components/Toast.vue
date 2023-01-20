1e01b36bc2280dfa38765442d371103367a40a5f
mason369 <1960638223@qq.com>
2023-01-20 17:53:30 +0800

    update:添加路由守卫

---
+++ src/components/Toast.vue
@@ -0,0 +1,62 @@
+<template>
+	<!-- 封装提示框组件 -->
+	<!-- 
+  看type的值:
+  success   iconfont icon-toast_chenggong
+  warning  iconfont icon-toast-jinggao 
+  danger   iconfont icon-toast-shibai_huaban
+ -->
+	<div class="toast">
+		<i
+			:class="
+				type == 'success'
+					? 'iconfont icon-toast_chenggong'
+					: type == 'warning'
+					? 'iconfont icon-toast-jinggao'
+					: 'iconfont icon-toast-shibai_huaban'
+			"
+		></i>
+		<span>{{ msg }}</span>
+	</div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+	data() {
+		return {};
+	},
+	computed: {
+		...mapState({
+			msg : (state) => state.showToast.msg,
+			type: (state) => state.showToast.type
+		})
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.toast {
+	background-color: #fff;
+	position: fixed;
+	left: 50%;
+	top: 0;
+	transform: translateX(-50%);
+	padding: 10px 20px;
+	border-radius: 6px;
+	box-shadow: 0 0 10px #fff;
+	i {
+		margin-right: 8px;
+	}
+
+	.icon-toast_chenggong {
+		color: green;
+	}
+	.icon-toast-jinggao {
+		color: orange;
+	}
+	.icon-toast-shibai_huaban {
+		color: red;
+	}
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,8 @@ import Vuex from "vuex";
 import isShowLoginModal from "@/store/isShowLoginModal";
 import loginStatus from "@/store/loginStatus";
 import userInfo from "@/store/userInfo";
+import showToast from "@/store/showToast";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({modules: { isShowLoginModal, loginStatus, userInfo }});
+export default new Vuex.Store({modules: { isShowLoginModal, loginStatus, userInfo, showToast }});
